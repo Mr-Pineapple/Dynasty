2cb01f465688e58a51915e3f740eca7c238ebd0d
Mr-Pineapple <zackbird1@outlook.com>
2021-06-06 21:14:28 +0100

    Reverted CoinPressBlock

--- src/main/java/co/uk/mrpineapple/dynasty/common/block/CoinPressBlock.java
+++ src/main/java/co/uk/mrpineapple/dynasty/common/block/CoinPressBlock.java
@@ -3,17 +3,12 @@ package co.uk.mrpineapple.dynasty.common.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.inventory.ISidedInventory;
-import net.minecraft.inventory.ISidedInventoryProvider;
-import net.minecraft.tileentity.FurnaceTileEntity;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorld;
 
 import javax.annotation.Nullable;
 
-public class CoinPressBlock extends Block implements ISidedInventoryProvider {
+public class CoinPressBlock extends Block {
     public CoinPressBlock() {
         super(Properties.copy(Blocks.STONE));
     }
@@ -29,14 +24,4 @@ public class CoinPressBlock extends Block implements ISidedInventoryProvider {
         return null;
     }
 
-    @Override
-    public ISidedInventory getContainer(BlockState state, IWorld world, BlockPos pos) {
-        if(!world.isClientSide()) {
-            TileEntity tileEntity = world.getBlockEntity(pos);
-            if(tileEntity instanceof FurnaceTileEntity) {
-                return (FurnaceTileEntity) tileEntity;
-            }
-        }
-        return null;
-    }
 }
