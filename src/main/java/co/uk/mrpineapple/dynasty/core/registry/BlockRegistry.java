eba26df684f3dfba8daef82ce8e61266e7df8873
Mr-Pineapple <zackbird1@outlook.com>
2021-06-07 13:35:20 +0100

    Registered BlockItems

--- src/main/java/co/uk/mrpineapple/dynasty/core/registry/BlockRegistry.java
+++ src/main/java/co/uk/mrpineapple/dynasty/core/registry/BlockRegistry.java
@@ -3,12 +3,22 @@ package co.uk.mrpineapple.dynasty.core.registry;
 import co.uk.mrpineapple.dynasty.common.block.CoinPressBlock;
 import co.uk.mrpineapple.dynasty.core.Dynasty;
 import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.function.Supplier;
+
 public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Dynasty.ID);
 
-    public static final RegistryObject<Block> COIN_PRESS = BLOCKS.register("coin_press", CoinPressBlock::new);
+    public static final RegistryObject<Block> COIN_PRESS = register("coin_press", CoinPressBlock::new);
+
+    public static <B extends Block> RegistryObject<B> register(String name, Supplier<? extends B> supplier) {
+        RegistryObject<B> block = BLOCKS.register(name, supplier);
+        ItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+        return block;
+    }
 }
